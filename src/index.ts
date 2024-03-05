7d153bc271e773ad2af9903a36208d6bc8246eb3
heren <heren1heren@gmail.com>
2024-03-05 15:10:10 -0700

    finish

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-// import './style.css';
-import './style.scss';
+// import LinkedList.ts and Node.ts here
+console.log('hello.');
+import node from './Node';
+import linkedList from './LinkedList';
